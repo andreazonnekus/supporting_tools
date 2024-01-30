8f50f458bd5a212fc5577c6cc51d9570536e73f8
Andrea Zonnekus <zonne@tutanota.de>
2024-01-30 21:52:29 +0800

    extract features from test

--- letter_ocr.py
+++ letter_ocr.py
@@ -1,14 +1,17 @@
 from PIL import Image
 from dotenv import load_dotenv
-import os, sys, pytesseract, cv2, pandas
+import os, sys, pytesseract, cv2, pandas, statistics
 from pytesseract import Output
 import numpy as np
 
 load_dotenv()
 
+MODEL_PATH = os.environ.get('MODEL_PATH')
+TESSERACT_PATH = os.environ.get('TESSERACT_PATH')
+
 def main() -> int:
     try:
-        pytesseract.pytesseract.tesseract_cmd = os.environ.get('TESSERACT_PATH')
+        pytesseract.pytesseract.tesseract_cmd = TESSERACT_PATH
         if (not os.path.exists(pytesseract.pytesseract.tesseract_cmd)) or (not os.path.isfile(pytesseract.pytesseract.tesseract_cmd)):
             raise Exception("Please supply a valid path for the Tesseract executable")
     except KeyError:
@@ -28,50 +31,80 @@ def prep(model):
     knn = cv2.ml.KNearest.load()
 
 def identify():
-    rect_d=[] #store the dimensions of my bounding boxes
+    shift = lambda x: x-8
+
+    features = pandas.DataFrame(
+        {
+            'x-box': [],
+            'y-box': [],
+            'width': [],
+            'high': [],
+            'onpix': [],
+            'x-bar': [],
+            'y-bar': [],
+            'x2bar': [],
+            'y2bar': [],
+            'xybar': [],
+            'x2ybr': [],
+            'xy2br': [],
+            'x-ege': [],
+            'xegvy': [],
+            'y-ege': [],
+            'yegvy': [],
+        }
+    )
+    feature_row, x_pos, y_pos = [], [], []
     final = ''
     min_cont_area=5
 
-    filename = os.path.join('assets', 'input','writing_only.jpg')
+    filename = os.path.join('assets', 'input','K.jpg')
     img = cv2.imread(filename)
     final_img = img.copy()
     mod_img = cv2.cvtColor(img.copy(), cv2.COLOR_BGR2GRAY)
-
-
-    # knn_model = cv2.ml.KNearest.load(os.path.join(os.environ.get("MODEL_PATH"),'knn_writing'))
-
-    # # mod_img = cv2.copyMakeBorder(img, 2, 2, 2, 2, borderType=cv2.BORDER_CONSTANT)
-    # # cv2.imshow("Output: ", mod_img)
-    # # cv2.imshow("Output: ", img)
-    # cv2.waitKey(0)
-
-    # mod_img = cv2.medianBlur(mod_img,3)
-    mod_img = cv2.bilateralFilter(src=mod_img, d=9, sigmaColor=9, sigmaSpace=7)
-    cv2.imshow("Output: ", mod_img)
-    cv2.waitKey(0)
-
-    mod_img = cv2.Canny(mod_img, 127, 255)
-
-    print(mod_img)
-    cv2.waitKey(0)
-
     mod_img = mod_img.astype('uint8')
 
-    other, mod_img = cv2.threshold(mod_img,160,255,cv2.THRESH_TOZERO+cv2.THRESH_OTSU)
+    thresh = cv2.threshold(mod_img, 146, 255, cv2.THRESH_BINARY)[1]
 
-    cv2.imshow("Thresh: ", mod_img)
-    cv2.waitKey(0)
+    # contours, _ = cv2.findContours(thresh, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
+    contours = cv2.findContours(thresh, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_NONE)[-2]
 
-    contours, _ = cv2.findContours(mod_img, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
-    cont_img = mod_img.copy()
+    cv2.drawContours(final_img, contours, -1, (255, 255, 255), cv2.FILLED);
+    
+    letter = pandas.DataFrame(np.asarray(thresh))
+    np.savetxt(os.path.join(MODEL_PATH, 'letter'), thresh, delimiter=',', fmt='%d')
+    onpix = int(np.count_nonzero(thresh==0)/17) # everything was scaled to 15 max
 
-    cv2.drawContours(final_img, contours, -1, (0, 0, 255), cv2.FILLED, 8);
+    x, x_bind, y_bind, last = letter.shape[1]-1, 0, 0, 0 # The first and last 
+    y = letter.shape[0]-1
+    
+    # inclusive height and width
+    height = y_bind-y
+    width = x_bind-x + 2 
 
-    # cv2.imshow(final_img)
+    zeros = np.where(letter == 0)
+    x_vals = shift(zeros[1])
+    y_vals = shift(zeros[0])
+
+    x_max, y_max = zeros[1].max(), 16 - zeros[0].min()
+    x, y = zeros[1].min(), 16 - zeros[0].max() - 1 # algorithm counts from the bottom
+
+    width = x_max - x + 1
+    height = y_max - y
+
+
+    if x and y:
+        feature_row.extend((x, y, width, height, onpix , int(x_vals.mean()), int(y_vals.mean()), int(np.square(x_vals).mean()), int(np.square(y_vals).mean())))
+        print(feature_row)
+    
+    # Width of box
+
+    # Heigth of box
+
+    # Amount of pixels in box
+    #feature_row.append(total_pixels)
     # cv2.imshow('Final Result',cont_img)
     cv2.imwrite('Final Result.jpg',final_img)
 
-    cv2.waitKey(0)
     cv2.destroyAllWindows()
 
     # ret,result,neighbours,dist = knn_model.findNearest(test,k=5)
