tidy model logistic model declaration, add gradient calc, fix estimator. implement bi-rnn, remove redundant softmax on cbow, add utilities for seq2seq

classes/cbow.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
-import random, pickle, os, torch
+import random, pickle, os
+from torch import sum, from_numpy
 import numpy as np
 from joblib import dump, load
 import torch.nn.functional as F
@@ -18,13 +19,13 @@ class CBOW(nn.Module):
         self.learning_rate = learning_rate
         self.iterations = num_iterations
 
-        self.linear1 = nn.Linear(classes_dim, embedding_dim * window_size) 
-        self.linear2 = nn.Linear(embedding_dim * window_size, classes_dim)
+        self.linear1 = nn.Linear(classes_dim, embedding_dim * window_dim) 
+        self.linear2 = nn.Linear(embedding_dim * window_dim, classes_dim)
         self.activation = nn.ReLU()
         self.loss = nn.CrossEntropyLoss()
     
     def forward(self, x):
-        l1 = self.linear1(torch.sum(x, dim=1))
+        l1 = self.linear1(sum(x, dim=1))
 
         # get the output of l1
         return self.linear2(l1)
@@ -85,14 +86,13 @@ for epoch in range(no_of_epochs):
 
     # shuffle the training set to make each epoch's batch different, you can also skip this step
     shuffle(cbow)
-    loss_sum = 0
 
     for ind in range(0, len(cbow),batch_size):
         data_temp = cbow[ind : min(ind+batch_size, len(cbow))]
-        inputs_temp, labels_temp = prepare_batch(data_temp, voc_size)
+        inputs_temp, labels_temp = prepare_cbow_batch(data_temp, voc_size)
         
-        inputs_torch = torch.from_numpy(inputs_temp).float()
-        labels_torch = torch.from_numpy(labels_temp).long()
+        inputs_torch = from_numpy(inputs_temp).float()
+        labels_torch = from_numpy(labels_temp).long()
         
         model.train() # mode = True by default
 
@@ -106,11 +106,9 @@ for epoch in range(no_of_epochs):
 
         optimiser.step() # back propagation
 
-        loss_sum += loss.item()
-
     if epoch % 500 == 499:
         print(labels_torch.shape, outputs.size())
-        print('Epoch: %d, loss: %.4f' %(epoch + 1, loss_sum))
+        print('Epoch: %d, loss: %.4f' %(epoch + 1, loss.item()))
 
 figure = generate_projections(word_list, model.embedding.weight.data)
 fig_path = os.path.join('assets', 'output')

classes/logistic_regression.py
@@ -14,47 +14,42 @@ class LogisticRegressionClassifier():
         self.learning_rate = learning_rate
         self.iterations = iterations
         self.verbose = verbose
+
+        # store calculations
+        self.probability, self.gradient, self.cost, self.ch = 0, 0, 0, []
+        self.theta = 0
+
+        # store this as a lambda function - Equation 4-13. Logistic Regression model estimated probability (vectorized form)
+        self.estimate_probability = lambda instance: 1/(1+(np.exp(-np.dot(instance, self.theta.T))))
         
-        # weight, bias
-        # make sure these accept floats, and don't just cast them to 0
-        self.probability, self.gradient, self.cost = 0, 0, []
-        self.theta = [ 0, 0 ]
-        # store this as a lmabda function - Equation 4-13. Logistic Regression model estimated probability (vectorized form)
-        # self.estimate_probability_singluar = lambda instance: 1/(1+(np.exp(-np.dot(self.theta[0],np.array(instance, dtype=np.float64).reshape(-1,1)) + self.theta[1])))
-        self.estimate_probability = lambda instance: 1/(1+(np.exp(-np.dot(self.theta[0], instance.T) + self.theta[1])))
-        # self.estimate_probability = lambda instance: 1/(1+(np.exp(-np.dot(instance,self.theta[0]))))
         # calculate the logistic regression cost function - Equation 4-16. Cost function of a single training instance
-        # self.predictor = lambda instance: np.expand_dims((self.estimate_probability(instance) >= 0.5).astype(int), axis=1)
         self.predictor = lambda instance: (self.estimate_probability(instance) >= 0.5).astype(int)
 
     def train(self, X, y):
         instances, feats = X.shape
         # self.gradient = np.zeros(instances, dtype=np.int64)
-        self.theta[0] = np.random.random((1, feats))
-        self.theta[1] = random.random()
+        self.theta = np.random.random((1, feats))
+        self.bias = np.random
 
         for epoch in range(0, self.iterations):
-            # ix = random.random(instances)
-            # x = X[ix]
-            # label = y[ix]
-
-            # TODO: calculate the gradient
-            # self.gradient = y.T - (np.dot(self.theta[0], X.T) + self.theta[1])
-            self.gradient = y.T - self.estimate_probability(X)
-            
-            # update theta
-            self.theta[0] -= self.learning_rate * -np.mean(np.dot(X.T, self.gradient.T)) 
-            self.theta[1] -= self.learning_rate * -np.mean(np.sum(self.gradient))
-
             # estimate probability
             self.probability = self.estimate_probability(X)
+            
+            # calculate the gradient
+            self.gradient = (np.dot(X.T, (self.probability - y)) / instances) * lr
+            
+            # update theta
+            self.theta -= self.gradient.T
 
             #calculate cost function
-            self.cost.append(np.mean(self.probability - y.T))
+            self.cost = np.asarray(((-y * np.log(self.probability) - ((1.0-y) * np.log(1.0-self.probability))).sum()/instances))[0]
+
+            # store cost function history
+            self.ch.append(self.cost)
 
             # output some training information,
-            # if abs(self.gradient - self.cost[-1]) <= 0.0001:
-            #     print(f'converged at {epoch}')
+            if len(self.ch) > 2 and abs(self.ch[-2] - self.cost) <= 0.0001:
+                print(f'converged at {epoch}')
             if epoch % 400 == 399 and self.verbose:
                 print(f'0:0.3%', self.cost)
 
@@ -62,9 +57,10 @@ lr = 0.01
 iterations = 10000
 model = LogisticRegressionClassifier(iterations, lr)
 
-x_train = pd.read_csv(os.path.join("assets", "train", 'FMNIST_training_set.csv'))
+# load and scale down
+x_train = pd.read_csv(os.path.join("assets", "train", 'FMNIST_training_set.csv'))/255
 y_train = pd.read_csv(os.path.join("assets", "train", 'FMNIST_training_set_labels.csv'))
-x_test = pd.read_csv(os.path.join("assets", "train", 'FMNIST_test_set.csv'))
+x_test = pd.read_csv(os.path.join("assets", "train", 'FMNIST_test_set.csv'))/255
 y_test = pd.read_csv(os.path.join("assets", "train", 'FMNIST_test_set_labels.csv'))
 
 # break this into a binary classification problem: 7 is sneaker, 5 is sandal

classes/utils.py
@@ -15,11 +15,13 @@ from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import FunctionTransformer, StandardScaler
 from sklearn.model_selection import train_test_split
+import torch
 
 load_dotenv()
 
 column_ratio = lambda X: X[:, 0] / X[:, 1]
 ratio_name = lambda transformer, feature_names_in: ['ratio']
+prepare_vocab_dict = lambda char_arr: {n: i for i, n in enumerate(char_arr)}
 
 def make_ratio_pipeline() -> Pipeline:
     return make_pipeline([
@@ -581,7 +583,10 @@ def prepare_cbow_batch(data_temp, voc_size):
 
     return np.array(inputs), labels
 
-def prepare_batch(data_temp, voc_size):
+def prepare_cbow_batch(data_temp, voc_size):
+    """
+    A preparatory function for 
+    """
     inputs = []
     labels = []
 
@@ -598,3 +603,53 @@ def prepare_batch(data_temp, voc_size):
         labels.append(data_temp[i][1])
 
     return np.array(inputs), np.array(labels, dtype = np.int64)
+
+def add_paddings(word, max_num=5):
+    diff = max_num - len(word)
+    return word+'P'*diff
+
+def make_seq2seq_batch(seq_data, max_word_len = 5, dict_len:int = None, char_array = None):
+
+    encoder_input_batch = []
+    decoder_input_batch = []
+    target_batch = []
+    # Generate unique tokens list
+
+    if not char_array:
+        chars = []
+        for seq in seq_data:
+            chars += list(seq[0])
+            chars += list(seq[1])
+        # To simplify the question, we put all characters (including input and output) into one set
+        char_array = list(set(chars))
+
+        # special tokens are required
+        # B: Beginning of Sequence
+        # E: Ending of Sequence
+        # P: Padding of Sequence - for different size input
+        # U: Unknown element of Sequence - for different size input
+        char_array.append('B')
+        char_array.append('E')
+        char_array.append('P')
+        char_array.append('U')
+
+    num_dict = prepare_vocab_dict(char_array)
+
+    if not dict_len:
+        dict_len = len(num_dict)
+
+    for seq in seq_data:
+        # Input for encoder cell, convert to vector
+        input_word = add_paddings(seq[0], max_word_len)
+        en_input_data = [num_dict[n] for n in input_word]
+        # Input for decoder cell, Add 'B' at the beginning of the sequence data
+        de_input_data  = [num_dict[n] for n in ('B'+ seq[1])]
+
+        target = [num_dict[n] for n in (seq[1] + 'E')]
+
+        # Convert each character vector to one-hot encoding data
+        encoder_input_batch.append(np.eye(dict_len)[en_input_data])
+        decoder_input_batch.append(np.eye(dict_len)[de_input_data])
+
+        target_batch.append(target)
+    return char_array, encoder_input_batch, decoder_input_batch, target_batch